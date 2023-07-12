66486b008f29234ee6f07d873f7d0151d12b5ee8
vkhydras <victorkimaru8@gmail.com>
2023-07-12 18:41:49 +0300

    code update 2

--- server.js
+++ server.js
@@ -7,16 +7,14 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-
-mongoose.connect("mongodb+srv://victorkimaru8:stvick.9028@cluster0.3clz1wb.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://victorkimaru8:<password>@cluster0.3clz1wb.mongodb.net/TicTacToe?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log('MongoDB connected')
+  console.log('MongoDB connected');
 }).catch((err) => {
   console.error('Error connecting to MongoDB:', err);
 });
-
 
 // Define the move schema
 const moveSchema = new mongoose.Schema({
@@ -25,7 +23,8 @@ const moveSchema = new mongoose.Schema({
     ref: 'Game'
   },
   player: String,
-  position: Number,
+  row: Number,
+  column: Number,
   timestamp: {
     type: Date,
     default: Date.now
@@ -56,7 +55,6 @@ const gameSchema = new mongoose.Schema({
 
 const Game = mongoose.model("Game", gameSchema);
 
-
 // Create a new game
 app.post("/games", async (req, res) => {
   const { board } = req.body;
@@ -71,7 +69,7 @@ app.post("/games", async (req, res) => {
     res.status(500).send("Error creating game");
   }
 });
-// Retrieve game data
+
 // Retrieve game data
 app.get("/games", async (req, res) => {
   try {
@@ -90,47 +88,51 @@ app.get("/games", async (req, res) => {
   }
 });
 
-// ...
-
-
-
-
-
-
-
-
-
 // Update game data and log moves
 app.put("/games", async (req, res) => {
   const { board } = req.body;
-  
+
   try {
-    const game = await Game.findOne({});
-    
+    let game = await Game.findOneAndUpdate(
+      {},
+      { $set: { board } },
+      { new: true }
+    );
+
     if (!game) {
       return res.status(404).send("Game not found");
     }
-    
-    game.board = board;
-    
-    const updatedGame = await game.save();
-    
-    const winner = checkWinner(updatedGame.board);
-    const status = winner ? "completed" : "ongoing";
-    
-    updatedGame.status = status;
-    updatedGame.winner = winner;
-    
-    await updatedGame.save();
-    
-    res.json(updatedGame);
+
+    const winner = checkWinner(game.board);
+    const status = winner ? "completed" : game.board.includes("") ? "ongoing" : "draw";
+
+    game.status = status;
+    game.winner = winner;
+
+    const position = board.findIndex((cell) => cell === "X"); // Find the position of the move
+    const row = Math.floor(position / 3); // Calculate the row index
+    const column = position % 3; // Calculate the column index
+
+    const move = new Move({
+      game: game._id,
+      player: "X", // Assuming player X makes the move
+      row,
+      column
+    });
+
+    await move.save();
+
+    // Update the moves array in the game
+    game.moves.push(move._id);
+
+    await game.save();
+
+    res.json(game);
   } catch (err) {
     console.error("Error updating game:", err);
     res.status(500).send("Error updating game");
   }
 });
-
-
 
 // Helper function to check for winning moves
 function checkWinner(board) {
@@ -154,36 +156,12 @@ function checkWinner(board) {
   return null; // No winner
 }
 
-// Delete a game
-app.delete("/games/:id", (req, res) => {
-  const gameId = req.params.id;
-
-  Game.findByIdAndRemove(gameId, (err) => {
-    if (err) {
-      console.error("Error deleting game:", err);
-      res.status(500).send("Error deleting game");
-    } else {
-      res.send("Game deleted successfully");
-    }
-  });
-});
-
 // Start the server
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
 
 // Helper function to create empty cells
-function createCells() {
-  const cellsArray = [];
-
-  for (let i = 0; i < 9; i++) {
-    cellsArray.push("");
-  }
-
-  return cellsArray;
-}
-
 function createCells() {
   const cellsArray = [];
 
